caf3c96745a73aa129fcac169a34411063e525c7
Bertie Wheen <wheen.b@gmail.com>
2015-02-15 05:59:16 +0000

    Break out example-language-specific code

--- src/Languages/SimpleState.hs
+++ src/Languages/SimpleState.hs
@@ -2,15 +2,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverlappingInstances #-}
 
-module ModularEvaluator where
+module Languages.SimpleState where
 
+import Control.Applicative (Applicative(..), (<$>))
+import Control.Monad.State (StateT(runStateT), get, lift, modify)
+import Modular.Evaluator
+import qualified Data.Map as Map
 import Prelude hiding (lookup)
 import qualified Prelude
-import Control.Applicative (Applicative(..), (<$>))
-import Control.Monad.State (StateT, get, modify, lift, runStateT)
-import qualified Data.Map as Map
 
 -- Value maps
 class ValueMap map k v where
@@ -30,66 +30,6 @@ instance Eq k => ValueMap AssocList k v where
 instance Ord k => ValueMap Map.Map k v where
   lookup = Map.lookup
   insert = Map.insert
-
--- Bifunctors
-class Bifunctor p where
-  bimap :: (a -> a') -> (b -> b') -> p a b -> p a' b'
-  bimap f g = first f . second g
-  first :: (a -> a') -> p a b -> p a' b
-  first f = bimap f id
-  second :: (b -> b') -> p a b -> p a b'
-  second g = bimap id g
-
-instance Bifunctor Either where
-  bimap f g = either (Left . f) (Right . g)
-
-instance Bifunctor (,) where
-  bimap f g (x, y) = (f x, g y)
-
--- Functor products/coproducts
-newtype Lift p f g a = Lift { runLift :: p (f a) (g a) }
-type (:*:) = Lift (,)
-type (:+:) = Lift Either
-inl = Lift . Left
-inr = Lift . Right
-
-instance (Bifunctor p, Functor f, Functor g) => Functor (Lift p f g) where
-  fmap f = Lift . bimap (fmap f) (fmap f) . runLift
-
--- Functor subtyping
-class (Functor f, Functor g) => f :<: g where
-  inj :: f a -> g a
-
-instance Functor f => f :<: f where
-  inj = id
-
-instance (Functor f, Functor g) => f :<: (f :+: g) where
-  inj = inl
-
-instance (Functor f, Functor g, Functor h, f :<: h) => f :<: (g :+: h) where
-  inj = inr . inj
-
-inject :: (g :<: f) => g (Fix f) -> Fix f
-inject = In . inj
-
--- Fixed-points
-newtype Fix f = In { out :: f (Fix f) }
-
-cata :: Functor f => (f a -> a) -> Fix f -> a
-cata f = f . fmap (cata f) . out
-
--- F-algebras
-type Algebra f a = f a -> a
-
--- Eval
-class Eval f m v where
-  evalAlgebra :: Algebra f (m v)
-
-eval :: (Functor f, Eval f m v) => Fix f -> m v
-eval = cata evalAlgebra
-
-instance (Eval f m v, Eval g m v) => Eval (f :+: g) m v where
-  evalAlgebra = either evalAlgebra evalAlgebra . runLift
 
 -- Expression types
 type Expr = Fix (ArithExpr :+: VarExpr)
